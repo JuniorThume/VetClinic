7b922eb14af8f1cc15877ba028ca7666d649d37e
Valmir Thume Junior <juninhothume21@gmail.com>
2024-05-08 18:22:05 -0300

    :sparkles: feat: Cria as rotas mas não implementa a lógica

---
+++ routes/pet.routes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+router.post('/pet/:tutorId', function (req, res) {
+
+})
+
+router.put('/pet/:petId/tutor/:tutorId', function (req, res) {
+
+})
+
+router.delete('/pet/:petId/tutor/:tutorId', function (req, res) {
+
+})
+
+module.exports = router;
---
+++ routes/tutor.routes.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+
+
+router.get('/tutors'/ function(req, res) {
+
+})
+
+router.post('/tutors', function(req, res) {
+
+})
+
+router.put('/tutors/:id', function(req, res) {
+
+})
+
+router.delete('/tutors/:id', function(req, res){
+
+})
+
+module.exports = router;
