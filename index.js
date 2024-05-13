b1d6d168fa5eaad98e91b449fc4bae68623a0b2a
Valmir Thume Junior <juninhothume21@gmail.com>
2024-05-13 11:43:24 -0300

    :recycle: refactor: Descentraliza o código do arquivo de entrada

--- index.js
+++ index.js
@@ -1,24 +1,33 @@
+require('dotenv').config();
+const port_db = process.env.PORT_DB
+
+// Configs basicas
 const express = require('express');
 const app = express();
-const PORT = 5000;
-const { Sequelize } = require('sequelize');
-const Op = Sequelize.Op;
 
+//body parser
 const bodyParser = require('body-parser');
-
 app.use(bodyParser.urlencoded({ extended: false }))
 
+// db
+const db = require('./db/connection')
+db
+    // .sync({force: true})
+    .sync()
+    .then(() => {
+        app.listen(port_db, function(err){
+            if(err) {
+                console.log(err.message);
+                process.exit(1);
+            }
+            console.log(`Listening on port ${port_db}`);
+        })
+        console.log("Authenticate successful");
+    })
+    .catch(() => {
+        console.log("Authenticate failure");
+    })
 
+app.use('/tutors', require('./routes/tutor.routes'));
+app.use('/pets', require('./routes/pet.routes'));
 
-
-
-
-
-
-app.listen(PORT, function(err){
-    if(err) {
-        console.log(err.message);
-        return
-    }
-    console.log(`Listening on port ${PORT}`);
-})
